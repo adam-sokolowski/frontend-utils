29f310b14e00c8fa03c8ac0e82bef672607a0042
Adam Sokołowski <adam.sokolowski@gmail.com>
2020-08-16 09:20:47 +0200

    feat(all): add linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,27 @@ module.exports = {
   extends: [
     "plugin:@typescript-eslint/recommended"
   ],
-  rules: {}
+  "rules": {
+    "sort-imports": [
+      "error",
+      {
+        "ignoreCase": false,
+        "ignoreDeclarationSort": false,
+        "ignoreMemberSort": false,
+        "memberSyntaxSortOrder": ["none", "all", "multiple", "single"]
+      }
+    ],
+    "object-curly-newline": [
+      "error",
+      {
+        "ObjectPattern": { "multiline": true, "minProperties": 4 },
+        "ImportDeclaration": { "multiline": true, "minProperties": 4 },
+        "ExportDeclaration": { "multiline": true, "minProperties": 4 }
+      }
+    ],
+    "object-property-newline": [
+      "error",
+      { "allowAllPropertiesOnSameLine": true }
+    ],
+  }
 };
